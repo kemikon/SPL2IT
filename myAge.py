db89cf0e22939ac1f7b7e427e955acbd80b2a3d8
Ostap Filipenko <info@ostapfilipenko.com>
2019-02-27 14:33:28 +0100

    App about The Age

---
+++ myAge.py
@@ -0,0 +1,7 @@
+name = input("What is your name")
+print("hello, " + name)
+age = input("How old are you")
+ageAsInt = int(age)
+print(ageAsInt)
+nextYear = ageAsInt+1
+print("Next Year you will be " ,nextYear , " years old")
