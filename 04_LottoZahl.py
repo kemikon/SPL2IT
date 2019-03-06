adc91fde3252f7b91ed1b88c8efe3ec80f9348e9
Ostap Filipenko <info@ostapfilipenko.com>
2019-03-06 14:08:48 +0100

    es passt alles

--- 04_LottoZahl.py
+++ 04_LottoZahl.py
@@ -6,14 +6,26 @@ for i in range(1,46):
 
 gezogeneZahlen = []
 random.seed()
-for i in range(1,7):
-    gezogeneZahlen.append(random.randint(1,45))
+#for i in range(1,7):
+#    gezogeneZahlen.append(random.randint(1,45))
+
+#for q in gezogeneZahlen:
+#    for w in gezogeneZahlen:
+#        if(w == q):
+#            gezogeneZahlen.remove(w)
+#            gezogeneZahlen.append(random.randint(1,45))
+
+for i in range (1,7):
+    i = random.randint(0,len(lottozahlen)-1)
+    x = lottozahlen.pop(i)
+    gezogeneZahlen.append(x)
+    
 
 
-for z in gezogeneZahlen:
-    for zahlOfLotto in lottozahlen:
-        if(z == zahlOfLotto):
-            lottozahlen.remove(z)
+#for z in gezogeneZahlen:
+#    for zahlOfLotto in lottozahlen:
+#        if(z == zahlOfLotto):
+#            lottozahlen.remove(z)
         
 gezogeneZahlen.sort()
 print(gezogeneZahlen)
---
+++ for-schleifen.py
@@ -0,0 +1,21 @@
+for i in 1,2,3,4,5:
+    print(i, end=", ")
+print()
+for i in -4,-3,-2,-1,0,1,2,3,4:
+    print(i, end=", ")
+print()
+for i in "Alex", "Klaus", 22, 45, "Peter", 69:
+    print(i, end=", ")
+print()
+for i in range(0,9):
+    print(i, end=", ")
+print()
+for i in range(1,17,4):
+    print(i, end=", ")
+print()
+for i in range(17,1, -4):
+    print(i, end=", ")
+print()
+namen = ["O", "S", "R", "H", "P"]
+namen.pop(2)
+print(namen)
